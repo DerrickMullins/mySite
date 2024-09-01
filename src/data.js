81ab7858986cd02cf5369da694d0233f991b2866
Derrick Mullins <djm2382@gmail.com>
2024-09-01 15:27:59 -0500

    replacing contact with website

--- src/data.js
+++ src/data.js
@@ -2,6 +2,6 @@
 export const contentData = [
     { name: "About-Me", content: "Originally from Richmond, VA. I currently live in Austin, TX. with my amazing wife Gill." },
     { name: "Experience", content: "I'm a Software Developer at General Motors." },
-    { name: "Education", content: "I studied at Old Dominion University and Champlain College." },
+    { name: "Education", content: "I studied Political Science at Old Dominion University and Software Development at Champlain College." },
     { name: "Website", content: "This React application is being hosted on a Rasberry Pi." }
 ]
