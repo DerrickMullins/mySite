cf07cc1b8dc5ff799d4caccf88f87f3620fcb64e
Derrick Mullins <djm2382@gmail.com>
2024-09-01 13:43:38 -0500

    fixing rel=noopener bug

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ function App() {
     <div className="App">
       <div className={`App ${selectedTheme}`}>
         <div class="button-container">
-          <a className={selectedTheme === 'dark-mode' ? 'github-button' : 'github-button-light'} href="https://github.com/DerrickMullins" target="_blank" rel="noopener" />
-          <a className={selectedTheme === 'dark-mode' ? 'linkedin-button' : 'linkedin-button-light'} href="https://www.linkedin.com/in/derrick-mullins-446779285" target="_blank" rel="noopener" />
-          <a className={selectedTheme === 'dark-mode' ? 'email-button' : 'email-button-light'} href="mailto:djm2382@gmail.com" target="_blank" rel="noopener"/>
+          <a className={selectedTheme === 'dark-mode' ? 'github-button' : 'github-button-light'} href="https://github.com/DerrickMullins" target="_blank" rel="noopener noreferrer" />
+          <a className={selectedTheme === 'dark-mode' ? 'linkedin-button' : 'linkedin-button-light'} href="https://www.linkedin.com/in/derrick-mullins-446779285" target="_blank" rel="noopener noreferrer" />
+          <a className={selectedTheme === 'dark-mode' ? 'email-button' : 'email-button-light'} href="mailto:djm2382@gmail.com" target="_blank" rel="noopener noreferrer"/>
         </div>
         <ToggleButton onClick={toggleTheme} />
         <div className="profile-container">
