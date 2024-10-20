54c6fd31951671a02577d0dcb3707ddca16a060e
Derrick Mullins <djm2382@gmail.com>
2024-10-20 18:52:42 -0500

    modifying data.js and removing sun icon

--- src/data.js
+++ src/data.js
@@ -3,5 +3,5 @@ export const contentData = [
     { name: "About-Me", content: "Originally from Richmond, VA. I currently live in Austin, TX. with my amazing wife Gill." },
     { name: "Experience", content: "I'm a Software Developer at General Motors." },
     { name: "Education", content: "I studied Political Science at Old Dominion University and Software Development at Champlain College." },
-    { name: "Website", content: "This React application is being hosted on a Rasberry Pi." }
+    { name: "Website", content: "This React application is hosted on a Raspberry Pi using Express.js, Docker, and Nginx." }
 ]
