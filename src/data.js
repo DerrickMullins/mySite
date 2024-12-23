f7d945822c45f4239a4c69138b4ba45a469a072e
Derrick Mullins <djm2382@gmail.com>
2024-12-23 11:49:47 -0600

    updating data/content

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 
 export const contentData = [
-    { name: "About-Me", content: "Originally from Richmond, VA. I currently live in Austin, TX. with my amazing wife Gill." },
-    { name: "Experience", content: "I'm a Software Developer at General Motors." },
-    { name: "Education", content: "I studied Political Science at Old Dominion University and Software Development at Champlain College." },
-    { name: "Website", content: "This React application is hosted on a Raspberry Pi using Express.js, Docker, and Nginx." }
+    { name: "About-Me", content: "I'm originally from Richmond, VA. but, I currently live in Austin, TX. with my amazing wife Gill." },
+    { name: "Experience", content: "I've been working at General Motors as a Front-End Software Developer since 04/2022." },
+    { name: "Education", content: "I gradudated with a BS in Political Science from Old Dominion University in 2014 and a BS in Software Development from Champlain College in 2021." },
+    { name: "Website", content: "This React application was originally hosted on a Raspberry Pi using Express.js, Docker, and Nginx. However, I moved and didn't want to reconfigure everything, so now it's hosted on GitHub Pages. Thank you GitHub!" }
 ]
