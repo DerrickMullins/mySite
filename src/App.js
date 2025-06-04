516792f7ad657d51352a13d54e9e921c4b010ab6
Derrick Mullins <djm2382@gmail.com>
2025-06-04 12:18:32 -0500

    Adding type writer component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css'
 import TabButton from './components/tabButton'
 import './components/tabButton.css'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { contentData } from './data.js'
 import Profile from './components/profile'
 import ToggleButton from './components/toggleButton'
 import './components/styles.css'
+import Typewriter from './components/typewriter';
+import './components/typewriter.css'
 
 
 function App() {
   const [selectedContent, setSelectedContent] = useState(false)
   const [selectedTheme, setSelectedTheme] = useState('dark-mode')
+  const [profileImageIsVisible, setprofileImageIsVisible] = useState(false)
 
   const toggleTheme = () => {
     setSelectedTheme(selectedTheme === 'dark-mode' ? 'light-mode' : 'dark-mode')
@@ -19,63 +22,82 @@ function App() {
   const handleSelect = (selectedButton) => {
     setSelectedContent(selectedButton)
   }
+
+  useEffect(() => {
+    handleSelect(contentData[0].content)
+    const imageTarget = document.querySelector('.profile-container')
+    window.addEventListener('scroll', (event) => {
+      const imageRect = imageTarget.getBoundingClientRect().top
+      if (imageRect <= window.innerHeight) {
+        setprofileImageIsVisible(true)
+      }
+      if (imageRect >= window.innerHeight) {
+        setprofileImageIsVisible(false)
+      }
+    })
+  }, [])
   
 
   return (
     <div className="App">
       <div className={`App ${selectedTheme}`}>
-        <div class="button-container">
-          <a className={selectedTheme === 'dark-mode' ? 'github-button' : 'github-button-light'} href="https://github.com/DerrickMullins" target="_blank" rel="noopener noreferrer" aria-label="GitHub"/>
-          <a className={selectedTheme === 'dark-mode' ? 'linkedin-button' : 'linkedin-button-light'} href="https://www.linkedin.com/in/derrick-mullins-446779285" target="_blank" rel="noopener noreferrer" aria-label="Linkedin"/>
-          <a className={selectedTheme === 'dark-mode' ? 'email-button' : 'email-button-light'} href="mailto:djm2382@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email"/>
-        </div>
-        <ToggleButton onClick={toggleTheme} />
-        <div className="profile-container">
-          <Profile />
-          <menu className="horizontal-menu">
-            <li>
-              <TabButton
-                selectedTheme={selectedTheme}
-                isSelected={selectedContent === contentData[0].content}
-                onClick={() => handleSelect(contentData[0].content)}
-              >
-                About Me
-              </TabButton>
-            </li>
-            <li>
-              <TabButton
-                selectedTheme={selectedTheme}
-                isSelected={selectedContent === contentData[1].content}
-                onClick={() => handleSelect(contentData[1].content)}
-              >
-                Experience
-              </TabButton>
-            </li>
-            <li>
-              <TabButton
-                selectedTheme={selectedTheme}
-                isSelected={selectedContent === contentData[2].content}
-                onClick={() => handleSelect(contentData[2].content)}
-              >
-                Education
-              </TabButton>
-            </li>
-            <li>
-              <TabButton
-                selectedTheme={selectedTheme}
-                isSelected={selectedContent === contentData[3].content}
-                onClick={() => handleSelect(contentData[3].content)}
-              >
-                Website
-              </TabButton>
-            </li>
-          </menu>
-          <div className={`content-card ${selectedTheme}`}>
-            <div className='content'>
-              {selectedContent ? (selectedContent) : "Hi, I'm Derrick Mullins. Welcome to my site!!!"}
-            </div>
+          <Typewriter
+              header="Derrick Mullins"
+              text="Hey there, welcome to my site!!!"
+              selectedTheme={selectedTheme}
+          />
+          <div className="button-container">
+              <a className={selectedTheme === 'dark-mode' ? 'github-button' : 'github-button-light'} href="https://github.com/DerrickMullins" target="_blank" rel="noopener noreferrer" aria-label="GitHub"/>
+              <a className={selectedTheme === 'dark-mode' ? 'linkedin-button' : 'linkedin-button-light'} href="https://www.linkedin.com/in/derrick-mullins-446779285" target="_blank" rel="noopener noreferrer" aria-label="Linkedin"/>
+              <a className={selectedTheme === 'dark-mode' ? 'email-button' : 'email-button-light'} href="mailto:djm2382@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email"/>
           </div>
-        </div>
+          <ToggleButton onClick={toggleTheme} />
+          <main className={`profile-container ${profileImageIsVisible ? "display-profile" : "hide-profile"}`}>
+          <Profile/>
+              <menu className="horizontal-menu">
+                  <li>
+                      <TabButton
+                          selectedTheme={selectedTheme}
+                          isSelected={selectedContent === contentData[0].content}
+                          onClick={() => handleSelect(contentData[0].content)}
+                      >
+                          About Me
+                      </TabButton>
+                  </li>
+                  <li>
+                      <TabButton
+                          selectedTheme={selectedTheme}
+                          isSelected={selectedContent === contentData[1].content}
+                          onClick={() => handleSelect(contentData[1].content)}
+                      >
+                          Experience
+                      </TabButton>
+                  </li>
+                  <li>
+                      <TabButton
+                          selectedTheme={selectedTheme}
+                          isSelected={selectedContent === contentData[2].content}
+                          onClick={() => handleSelect(contentData[2].content)}
+                      >
+                          Education
+                      </TabButton>
+                  </li>
+                  <li>
+                      <TabButton
+                          selectedTheme={selectedTheme}
+                          isSelected={selectedContent === contentData[3].content}
+                          onClick={() => handleSelect(contentData[3].content)}
+                      >
+                          Website
+                      </TabButton>
+                  </li>
+              </menu>
+              <div className={`content-card ${selectedTheme}`}>
+                  <div className='content'>
+                      {selectedContent}
+                  </div>
+              </div>
+          </main>
       </div>
     </div>
   );
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,12 +1,12 @@
 import "./profile.css"
 import myLogo from '../assets/oysterMe.jpg'
 
-export default function Profile(){
+export default function Profile({profileImageIsVisible}){
     return (
         <img
-            className="profile-image"
+            className={`profile-image`}
             src={myLogo}
-            alt="meAndG"
+            alt="profile-image"
          />
     )
 }
