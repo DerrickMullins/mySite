e5c4697c3afd4431bd4896d6df984041973d1548
Derrick Mullins <djm2382@gmail.com>
2025-07-31 14:19:07 -0500

    Pushing to main to update gh-pages

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 
 export const contentData = [
-    { name: "About-Me", content: "I'm originally from Richmond, VA but, I currently live in Austin, TX with my amazing wife Gill and son Keanu." },
-    { name: "Experience", content: "I've been working at General Motors as a Software Developer since 04/2022." },
+    { name: "About-Me", content: "Originally from Richmond, VA. I currently live in Austin, TX. with my amazing wife Gill and son Keanu. We're planning on moving to Dublin, Ireland in the next couple of months." },
+    { name: "Experience", content: "I worked as a Software Developer at General Motors from April 2022 to August 2025. I am currently seeking a new Software Developer role in the Dublin area." },
     { name: "Education", content: "I gradudated with a BS in Political Science from Old Dominion University in 2014 and a BS in Software Development from Champlain College in 2021." },
-    { name: "Hobbies", content: "I'm a soccer nerd and a huge Chelsea fan. When I'm not working I like to travel and tinker." }
+    { name: "Hobbies", content: "I'm a football nerd and a huge Chelsea fan. When I'm not working I like to travel, hike, and play football." }
 ]
