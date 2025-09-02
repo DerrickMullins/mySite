80e13234b47b5ca796fe8758db6990dd33939780
Derrick Mullins <djm2382@gmail.com>
2025-09-02 14:41:51 -0500

    Revert "updating data"
    This reverts commit 8804917677a682f53dada468818d33e252d0bf06.

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 
 export const contentData = [
-    { name: "About-Me", content: "I'm originally from Richmond, VA. I currently live in Dublin Ireland. with my amazing wife Gill and son Keanu." },
+    { name: "About-Me", content: "Originally from Richmond, VA. I currently live in Dublin Ireland. with my amazing wife Gill and son Keanu." },
     { name: "Experience", content: "I worked as a Software Developer at General Motors from April 2022 to August 2025. I am currently seeking a new Software Developer role in the Dublin area." },
-    { name: "Education", content: "I gradudated with a degree in Political Science from Old Dominion University in 2014 and a BS in Software Development from Champlain College in 2021." },
+    { name: "Education", content: "I gradudated with a BS in Political Science from Old Dominion University in 2014 and a BS in Software Development from Champlain College in 2021." },
     { name: "Hobbies", content: "I'm a football nerd and a huge Chelsea fan. When I'm not working I like to travel, hike, and play football." }
 ]
