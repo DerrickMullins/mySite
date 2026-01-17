04faa1b198c7440562650d4ea88244d6e9640884
Derrick Mullins <djm2382@gmail.com>
2026-01-17 14:46:24 +0000

    updating data

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 
 export const contentData = [
     { name: "About-Me", content: "I'm originally from Richmond, VA! I currently live in Dublin Ireland with my amazing wife Gill and son Keanu." },
-    { name: "Experience", content: "I worked as a Software Developer at General Motors from April 2022 to August 2025. I'm currently working as a Web Developer at Peninsula Ireland." },
+    { name: "Experience", content: "I previously worked as a Software Developer at General Motors from April 2022 to August 2025. I'm currently working as a Web Developer at Peninsula Ireland." },
     { name: "Education", content: "I graduated with a degree in Political Science from Old Dominion University in 2014 and a BS in Software Development from Champlain College in 2021." },
     { name: "Hobbies", content: "I'm a football nerd and a huge Chelsea fan. When I'm not working I like to travel, hike, and play football." }
 ]
