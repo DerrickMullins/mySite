446b324f57ec1d52cb8e8e397b4d8ec40cbfccab
Derrick Mullins <djm2382@gmail.com>
2026-01-23 13:22:48 +0000

    first commit v2

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import './components/typewriter.css'
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import ProgressBar from "react-scroll-progress-bar";
 
 function App() {
     const [selectedContent, setSelectedContent] = useState(false)
@@ -54,15 +55,11 @@ function App() {
         observer.observe(imageTarget)
     }, [])
 
-    // scroll to top of page on page refresh
-    useEffect(() => {
-        window.history.scrollRestoration = 'manual'
-    })
+
 
     return (
         <div className={`App ${selectedTheme}`}>
-            <div class="scroll-progress"/>
-
+            <ProgressBar bgcolor={selectedTheme === 'dark-mode' ? 'purple' : 'black'}  duration='0.2'/>
             <div className="button-container">
                 <EmailOutlinedIcon fontSize='large' onClick={handleEmailClick} style={{ color: selectedTheme === "dark-mode" ? 'white' : 'black', cursor: "pointer" }} />
                 <LinkedInIcon fontSize='large' onClick={handleLinkedinClick} style={{ color: selectedTheme === "dark-mode" ? 'white' : 'black', cursor: "pointer" }} />
